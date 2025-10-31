a9a9d89703cca70ce4bfe47e36549e5eba167c3f
romeo <rbesar210311@gmail.com>
2025-10-31 17:59:33 +0000

    recovery: Show build version | version codename in recovery
    Signed-off-by: romeo <rbesar210311@gmail.com>
    Signed-off-by: Romeo <rbesar210311@gmail.com>

--- recovery.cpp
+++ recovery.cpp
@@ -878,16 +878,26 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
     ui->SetStage(st_cur, st_max);
   }
 
-  std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.afterlife.version", "(unknown)"),
-  };
-  title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
-  if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
+  std::string short_ver = android::base::GetProperty("ro.afterlife.build.version", "(unknown)");
+std::string codename = android::base::GetProperty("ro.afterlife.version.codename", "");
+std::string ver = short_ver;
+if (!codename.empty()) {
+    ver += " | " + codename;
+}
+
+std::vector<std::string> title_lines = {
+    "Version: " + ver,
+};
+
+title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
+
+if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
     std::string slot = android::base::GetProperty("ro.boot.slot_suffix", "");
     if (android::base::StartsWith(slot, "_")) slot.erase(0, 1);
     title_lines.push_back("Active slot: " + slot);
-  }
-  ui->SetTitle(title_lines);
+}
+
+ui->SetTitle(title_lines);
 
   ui->ResetKeyInterruptStatus();
   device->StartRecovery();
